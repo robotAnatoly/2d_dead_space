740e900e049235603ddc559c19f5d22bcbc7eb75
misteriusdrh <misteriusdrh@yandex.ru>
2016-05-29 22:26:37 -0400

    first commit

---
+++ app/src/main/java/alexey/a2d_dead_space/MainActivity.java
@@ -0,0 +1,15 @@
+package alexey.a2d_dead_space;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
